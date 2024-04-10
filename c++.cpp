c0026530eb1ee2c765dc8027ce3a7ec8268bbbe5
Cheasanna <165552353+Cheasanna@users.noreply.github.com>
2024-04-10 20:30:50 +0700

    Add files via upload

--- c++.cpp
+++ c++.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+  int Number1;
+  int Number2;
+  int Total;
+  cout << "\nplease input the Banana :";
+  cin >> Number1;
+  cout << "\input the Banana :";
+  cin >> Number2;
+  Total = Number1 + Number2;
+  cout <<"total :" << Total;
+  
+  return 0;
+}
